58b9e8a23148e66af9ef75c390aba2af8518dea7
Hamidur Rahman <Hamidur0405@gmail.com>
2023-09-04 13:56:09 -0400

    updated pom and added java code for multiple purposes

---
+++ src/main/java/com/hamidur/test/java/Main.java
@@ -0,0 +1,9 @@
+package com.hamidur.test.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Running a packaged java class which is part of the Spark/Scala.");
+    }
+
+}
